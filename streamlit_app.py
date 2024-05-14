ddfa8254993f1844bde1f3e0d5e25fb5ae964efb
SabihMalick123 <167012117+SabihMalick123@users.noreply.github.com>
2024-05-15 03:43:18 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ models = {
     "Llama2-7B": "meta/llama-2-7b-chat"
 }
 
-# os.environ['REPLICATE_API_TOKEN'] = "r8_1C6Ch8IUEXCHpnv5zF25jXCeYwAeLDP3zWjM6"
+os.environ['REPLICATE_API_TOKEN'] = "r8_UhPq7KWotNkc8JscYADAfA8pzOfiZxH048way"
 
 def select_model():
     return st.sidebar.selectbox('Choose a Model', list(models.keys()))
